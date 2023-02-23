2a50cd67e6923387c2e6d649ac77f35ed01c507f
lopo <lopo@zju.edu.cn>
2023-02-23 15:22:47 +0800

    feat: from json

--- src/ClassGenerator.vue
+++ src/ClassGenerator.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import { DartClassGenerator } from "./scripts";
+
+const source = ref('')
+const result = ref('')
+
+const doGenerate = () => {
+    if(source.value.trim() === '') return;
+
+    const r = DartClassGenerator.parse('json', source.value)
+    console.log(source.value, r)
+    result.value = r
+}
 
 </script>
 
 <template>
     <div class="class-generator">
-        13213
+        <textarea v-model="source" class="source" placeholder="source"></textarea>
+        <div class="btn" @click="doGenerate">generate!</div>
+        <textarea v-model="result" class="result" placeholder="result"></textarea>
     </div>
 </template>
 
@@ -13,6 +28,30 @@
     position: relative;
     width: 100%;
     height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+
+    .source, .result {
+        width: calc(50% - 100px);
+        height: calc(100% - 200px);
+        border: solid 1px #ccc;
+        border-radius: 8px;
+        outline: none;
+        padding: 16px;
+        resize: none;
+    }
+
+    .btn {
+        width: 120px;
+        height: 32px;
+        border: solid 1px #ccc;
+        border-radius: 8px;
+        color: #777;
+        text-align: center;
+        line-height: 32px;
+        cursor: pointer;
+    }
 
 }
 </style>
---
+++ src/scripts/index.ts
@@ -0,0 +1,104 @@
+// 来源
+export type SourceType = 'json' | 'ts-interface' | 'ts-type' | 'raw'
+
+// 能转换的类型
+type SupportedType = 'String' | 'bool' | 'int' | 'double' | 'dynamic'
+type ClassEntries = { name: string, type: SupportedType }[]
+
+const transType = (v: any): SupportedType => {
+    switch(typeof v) {
+        case 'string':
+            return 'String'
+        case 'boolean':
+            return 'bool'
+        case 'number':
+            return (v % 1 === 0) ? 'int' : 'double'
+        default:
+            return 'dynamic'
+    }
+}
+
+const codeTemplate = (className: string, entries: ClassEntries) => {
+    return `// auto generated.
+import 'dart:convert';
+
+class ${ className } {
+${ entries.map(({ name, type }) => `${ type }? ${ name };`).join('\n') }
+
+  ${ className }({
+${ entries.map(({ name }) => `this.${ name },`).join('\n') }
+  });
+
+  ${ className }.fromMap(Map map) {
+${ entries.map(({ name }) => `${ name } = map['${ name }'];`).join('\n') }
+  }
+
+  ${ className }.fromJson(String json) {
+    Map map = {};
+
+    try {
+      map = jsonDecode(json);
+    } catch (err) {
+      print('invalid json. $err');
+    }
+    
+    ${ className }.fromMap(map);
+  }
+
+  Map toMap() {    
+    return {
+${ entries.map(({ name }) => `'${ name }': ${ name },`).join('\n') }
+    };
+  }
+
+  String toJson() {
+    return jsonEncode(toMap());
+  }
+}
+`
+}
+
+abstract class DartClassGenerator {
+    private static fromJson(source: string, className: string = 'TemplateClass') {
+        try {
+            const obj = JSON.parse(source)
+            if(!(obj instanceof Object)) return 'TypeError'
+
+            const entries: ClassEntries = Object.entries(obj)
+                .map(([ key, val ]) => ({ name: key, type: transType(val) }))
+            return codeTemplate(className, entries)
+        } catch (err) {
+            console.log('[fromJson]: ', err)
+            return 'Internal Error'
+        }
+    }
+
+    private static fromTsInterface(source: string, className: string = 'TemplateClass') {
+
+    }
+
+    private static fromTsType(source: string, className: string = 'TemplateClass') {
+
+    }
+
+    private static fromRaw(source: string, className: string = 'TemplateClass') {
+
+    }
+
+    public static parse(sourceType: SourceType, source: string, className: string = 'TemplateClass') {
+        switch(sourceType) {
+            case 'json':
+                return DartClassGenerator.fromJson(source, className)
+            case 'ts-interface':
+                return DartClassGenerator.fromTsInterface(source, className)
+            case 'ts-type':
+                return DartClassGenerator.fromTsType(source, className)
+            case 'raw':
+                return DartClassGenerator.fromRaw(source, className)
+        }
+    }
+}
+
+export {
+    DartClassGenerator
+}
